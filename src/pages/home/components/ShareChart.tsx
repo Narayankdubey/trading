0a14a8bd59eb6f8b805f97c5158c50a338acb1be
Narayan Dubey <narayan.dubey@smarterp.com>
2023-04-27 11:13:49 +0530

    removed antd chart

--- src/pages/home/components/ShareChart.tsx
+++ src/pages/home/components/ShareChart.tsx
@@ -1,38 +1,12 @@
 import { Card } from 'antd'
-import React, {useState,useEffect, FC } from 'react'
-import { Stock } from '@ant-design/plots';
+import React, { FC } from 'react'
 
-interface ShareChartProps {
-  
-}
+interface ShareChartProps {}
 
 export const ShareChart: FC<ShareChartProps> = ({  }) => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    asyncFetch();
-  }, []);
-
-  const asyncFetch = () => {
-    setLoading(true)
-    fetch('https://gw.alipayobjects.com/os/antfincdn/qtQ9nYfYJe/stock-data.json')
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => {
-        console.log('fetch data failed', error);
-      });
-      setLoading(false)
-  };
-  const config = {
-    data,
-    xField: 'trade_date',
-    yField: ['open', 'close', 'high', 'low'],
-  };
   return (
-    <Card loading={loading}>
-        {/* @ts-ignore */}
-     <Stock {...config} />
+    <Card>
+      Chart Will be here
     </Card>
   )
 }
