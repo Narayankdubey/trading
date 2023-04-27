eabb161b80b29233778d0c89a2baa14d3947cdaa
Narayan Dubey <narayan.dubey@smarterp.com>
2023-04-28 00:35:32 +0530

    added condition on nav button

--- src/components/layout/components/BottomNav.tsx
+++ src/components/layout/components/BottomNav.tsx
@@ -1,26 +1,31 @@
 import { Button, Card, Col, Row, Space, Switch } from 'antd'
-import React, { Dispatch, FC, SetStateAction } from 'react'
+import React, { Dispatch, FC, SetStateAction, useState } from 'react'
 
 interface BottomNavProps {
   setDartMode: Dispatch<SetStateAction<boolean>>;
 }
 
+const menus = [
+  { name: "trading", title: "Trading", icon: "", },
+  { name: "", title: "Alog", icon: "", },
+  { name: "", title: "Backtesting", icon: "", },
+  { name: "", title: "$ 2,45,000", icon: "", },
+  { name: "", title: "Notes", icon: "", },
+  { name: "", title: "Logs", icon: "", },
+]
 export const BottomNav: FC<BottomNavProps> = ({ setDartMode }) => {
-  const menus = [
-    { name: "trading", title: "Trading", icon: "", },
-    { name: "", title: "Alog", icon: "", },
-    { name: "", title: "Backtesting", icon: "", },
-    { name: "", title: "$ 2,45,000", icon: "", },
-    { name: "", title: "Notes", icon: "", },
-    { name: "", title: "Logs", icon: "", },
-  ]
+  const [selected, setSelected] = useState("Trading");
+
+  const menuPressed = (name:string)=>{
+    setSelected(name);
+  }
   return (
     <Card size="small">
       <Row justify={"space-between"} align={"middle"}>
         <Col>
           <Space>
             {menus.map((item) => (
-              <Button key={item.title} size={"large"}>{item.title}</Button>
+              <Button key={item.title} size={"large"} type={selected === item?.title ? "primary" : "default"} onClick={()=>menuPressed(item?.title)}>{item.title}</Button>
             ))}
           </Space>
         </Col>
